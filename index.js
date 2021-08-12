7fb731831d38ba56eef21a50309ba3bc936fcb8d
Dawson Conway <dconman02@gmail.com>
2021-08-12 12:04:42 -0500

    Switch to a worker dyno

--- index.js
+++ index.js
@@ -6,11 +6,3 @@ const connectToDiscord = require('./discord-client/initialize');
 initDb()
   .then(() => connectToDiscord())
   .catch((e) => console.error(e.stack));
-
-const server = net.createServer();
-server.on('error', (err) => {
-  throw err;
-});
-server.listen(process.env.PORT, '0.0.0.0', () => {
-  console.log('server bound');
-});
