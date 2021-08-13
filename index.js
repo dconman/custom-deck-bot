e0632d76834f5d00836a5b61f632e0e31fe2a21c
Dawson Conway <dconman02@gmail.com>
2021-08-13 17:13:38 -0500

    propogate error on startup

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 process.env.TEST_MODE = Boolean(process.env.TEST_MODE);
-const net = require('net');
 const initDb = require('./db/initialize');
 const connectToDiscord = require('./discord-client/initialize');
 
 initDb()
   .then(() => connectToDiscord())
-  .catch((e) => console.error(e.stack));
+  .catch((e) => {
+    console.error(e.stack)
+    throw e;
+  });
